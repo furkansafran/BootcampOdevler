583e947f3aeb4dd42f33a26e957c7fcf7ea17f15
Furkan Safran <115262095+furkansafran@users.noreply.github.com>
2025-04-25 01:22:54 +0300

    odev1
    1.odev

---
+++ Odev1.kt
@@ -0,0 +1,96 @@
+package com.furkansafran.kotlindersleri.degiskenler
+
+fun main() {
+    val sehir = "Istanbul"
+    println("sehir : $sehir")
+    val ulke = "Turkiye"
+    println("ulke : $ulke")
+    val telefon = "05999999999"
+    println("telefon : $telefon")
+    val posta_kodu = "99999"
+    println("posta_kodu : $posta_kodu")
+    val email = "bootcamp@gmail.com"
+    println("email : $email")
+    val meslek = "Bilgisayar Muhendisi"
+    println("meslek : $meslek")
+    val stokMiktari = 99999
+    println("stokMiktari : $stokMiktari")
+    val musteri_adi = "Bootcamp"
+    println("musteri_adi : $musteri_adi")
+    val bakiye = 99999
+    println("Bakiye : $bakiye Tl")
+    val dogumGunu = "99/99/9999"
+    println("dogumGunu : $dogumGunu")
+    val maas = 99999
+    println("Maas : $maas Tl")
+    val medeniDurum = "Bekar"
+    println("medeni durumu : $medeniDurum")
+    val urun_yorum = "Muhteşem ürün"
+    println("yorum : $urun_yorum")
+    val odeme_tarihi = "99/99/9999"
+    println("odeme tarihi : $odeme_tarihi")
+    val odeme = false
+    println("odeme durumu : $odeme")
+    val siparisAdedi = 99999
+    println("siparis adedi : $siparisAdedi")
+    val araba_modeli = "Ford Focus"
+    println("araba modeli : $araba_modeli")
+    val kitapAdi = "KasimAdalan"
+    println("kitap adi : $kitapAdi")
+    val yayinlamaTarihi = "99/99/9999"
+    println("yayimlanma tarihi : $yayinlamaTarihi")
+    val indirim_miktari = 500
+    println("indirim miktari : $indirim_miktari Tl")
+    val odaSayisi = 99999
+    println("oda sayisi : $odaSayisi")
+    val enlem = 99.99
+    println("enlem : $enlem")
+    val boylam = 99.999
+    println("boylam : $boylam")
+    val urun_adi = "Kalem"
+    println("urun adi : $urun_adi")
+    val yemekFiyati = 99999
+    println("yemek fiyati : $yemekFiyati Tl")
+    val marka = "Kasim Adalan"
+    println("marka : $marka")
+    val muzik_adi = "Le Beyrut"
+    println("muzik adi : $muzik_adi")
+    val videoSuresi = "14:30"
+    println("video suresi : $videoSuresi")
+    val urunPuani = 4.5f
+    println("urun puani : $urunPuani")
+    val resimAdi = "KasimAdalan"
+    println("resim adi : $resimAdi")
+    val dosyaFormati = ".zip"
+    println("dosya formati : $dosyaFormati")
+    val renk = "mavi"
+    println("renk : $renk")
+    val renkKodu = "#FFFFFFF"
+    println("renk kodu: $renkKodu")
+    val telefonModeli = "Nokia 3310"
+    println("telefonModeli : $telefonModeli")
+    val ekran_boyutu = "1'"
+    println("ekran boyutu : $ekran_boyutu")
+    val agirlik = 99999
+    println("agirlik : $agirlik kg")
+    val ulusalGun = "23/04/2025"
+    println("ulusal gun : $ulusalGun")
+    val tatilGunu = "99/99/9999"
+    println("tatil gunu : $tatilGunu")
+    val rezervasyonTarihi = "99/99/9999"
+    println("rezervasyon tarihi : $rezervasyonTarihi")
+    val sokakAdi = "kale"
+    println("sokak adi : $sokakAdi")
+    val otobusHatti = "97"
+    println("otobus hatti : $otobusHatti")
+    val kalanDakika = 60
+    println("kalan dakika : $kalanDakika")
+    val takipKodu = "123123"
+    println("takip kodu : $takipKodu")
+    val kuponSuresi = "99/99/9999"
+    println("kupon suresi : $kuponSuresi")
+    val kupon_kodu = "99999"
+    println("kupon kodu : $kupon_kodu")
+    val fatura_adresi = "asdasd/asdasd/asdasdsa"
+    println("fatura adresi : $fatura_adresi")
+}
